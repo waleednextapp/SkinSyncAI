af329882f7a9afffdd10c1047ee25feb187f6318
waleednextapp <waleed.jawaid@nextappinc.com>
2025-05-29 00:58:11 +0500

    initial route changed

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -60,7 +60,7 @@ const withFAB = (ScreenComponent, navigation) => {
 const MainStack = () => {
   return (
     <Stack.Navigator
-      initialRouteName="Scanner"
+      initialRouteName="Splash"
       screenOptions={{headerShown: false}}>
       <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="GetStarted" component={GetStartedScreen} />
